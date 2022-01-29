f95145655cb1ddbf37dead6a5e8cec16b1024aa4
Saurabh Chopra <Saurabh.Chopra.2021@live.rhul.ac.uk>
2022-01-29 14:14:42 +0000

    add 217, add pre-commit

---
+++ 217. Contains Duplicate/solution.py
@@ -0,0 +1,22 @@
+# (if n denotes to counts of numbers in nums)
+# Time Complexity: O(n)
+# ... In each iteration, search and insertion of the hash set both takes
+# ... O(logn) time in worst case and O(1) time in average cases
+# ... Therefore, it’s takes O(n * (2logn)) = O(nlogn) time in worst case
+# ... but O(n) time in average cases.
+# Space Complexity: O(n)
+# ... it uses O(n) extra space for maintaining this hash set
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash_set = set()
+
+        for n in nums:
+            if n in hash_set:
+                return True
+
+            hash_set.add(n)
+
+        return False
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 I will be solving [LEETCODE PATTERNS](https://seanprashad.com/leetcode-patterns/)
 
 - [x] [217. Contains Duplicate](https://leetcode.com/problems/contains-duplicate/)
-- [ ] 
+- [ ]
